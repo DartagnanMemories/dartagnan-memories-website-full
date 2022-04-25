5e8583eead0fa0c1cbc3f165e2ac1b4bd1ee9a8c
larenelg <lotusgenome@gmail.com>
2022-04-25 17:42:48 +1000

    add poem

--- pages/memories/poems.vue
+++ pages/memories/poems.vue
@@ -1,9 +1,35 @@
 <template>
-  <Article title="Poems" />
+  <Article title="Poems">
+    <div
+      v-for="poem in poems"
+      :key="poem.createdAt"
+      class="pt-2 pb-8 border-t-2"
+    >
+      <div class="flex gap-4 items-baseline">
+        <h2 :id="poem.slug">
+          {{ poem.title }}
+        </h2>
+        <a v-if="$route.hash == `#${poem.slug}`" href="#toc"> Back to top</a>
+      </div>
+      <div class="mb-4">
+        <p class="font-semibold">From {{ poem.author }}</p>
+      </div>
+      <nuxt-content :document="poem" />
+    </div>
+  </Article>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $content }) {
+    const poems = await $content('poems')
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return {
+      poems
+    }
+  },
   head () {
     return {
       title: 'Poems'
