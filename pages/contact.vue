81aa00ef88818237a3d117647c61e322897fbbcb
larenelg <lotusgenome@gmail.com>
2022-04-26 14:04:36 +1000

    add text to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,6 +6,9 @@
   >
     <div class="mb-4">
       <p>D’artagnan was loved by many. Anyone who has a recollection about D’artagnan that they would like to share, please email your anecdotes, messages, photos, or any questions to <a href="mailto:messages@DartagnanMemories.og">messages@DartagnanMemories.org</a>.</p>
+      <p>Let us share our memories of D’artagnan with the world.</p>
+      <p>Thank you</p>
+      <p>Gisele</p>
     </div>
   </Article>
 </template>
