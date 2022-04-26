d176e6ae44c77a7b6252782e032b15fbaa722295
larenelg <lotusgenome@gmail.com>
2022-04-26 16:01:02 +1000

    add gallery

--- pages/memories/childrens-corner.vue
+++ pages/memories/childrens-corner.vue
@@ -1,17 +1,39 @@
 <template>
   <Article title="Children's Corner">
     <div class="mb-4">
-      <p>D’artagnan loved children, and children loved him.  This corner of the website is for children to share their tributes to D’artagnan: paintings, drawings, poems, stories or imaginings.</p>
+      <p>D’artagnan loved children, and children loved him. This corner of the website is for children to share their tributes to D’artagnan: paintings, drawings, poems, stories or imaginings.</p>
       <p>If it’s a painting or drawing, please describe it in words so that people who are blind can picture in their mind what you have painted or drawn.</p>
       <p>Please include first name and age. If you wish, you can also include suburb or country.</p>
       <p>Thank you</p>
       <p>Gisele</p>
+    </div>
+    <div class="mb-4">
+      <h2>Paintings and drawings</h2>
+      <div class="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-y-0 sm:gap-x-6 lg:gap-x-8">
+        <div v-for="photo in photos" :key="photo.slug">
+          <div class="w-full h-96 rounded-lg overflow-hidden group-hover:opacity-75 sm:h-auto sm:aspect-w-2 sm:aspect-h-3">
+            <img :src="require(`/assets/gallery/photos/${photo.img}`)" :alt="photo.title" class="w-full h-full object-center object-cover" />
+          </div>
+          <h3 class="my-4 text-base font-semibold text-gray-900">
+            {{ photo.title }}
+          </h3>
+        </div>
+      </div>
     </div>
   </Article>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $content }) {
+    const photos = await $content('childrens')
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return {
+      photos
+    }
+  },
   head () {
     return {
       title: 'Children\'s Corner'
--- pages/memories/gallery/photos.vue
+++ pages/memories/gallery/photos.vue
@@ -3,11 +3,33 @@
     <div class="mb-4">
       <p>I am blind, but I am aware of the powers of photos in capturing memories and telling a story. In this photo gallery we will post photos that show D’artagnan at various stages of his life and in different scenarios. During a computer upgrade in 2019, I lost many photos and other mementos of D’artagnan’s life, so I am reaching out to each of you to please search through photo files and send any photo you may have of D’artagnan. Please add a text description of any photo you send. Larene from Intopia who has been working hard to create this website is taking pains to ensure that we make the photo gallery as accessible as possible. We will try and add a narrative for the photos as we continue to recreate D’artagnan’s life on this tribute website.</p>
     </div>
+    <div class="mb-4">
+      <h2>D'artagnan and friends</h2>
+      <div class="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-y-0 sm:gap-x-6 lg:gap-x-8">
+        <div v-for="photo in photos" :key="photo.slug">
+          <div class="w-full h-96 rounded-lg overflow-hidden group-hover:opacity-75 sm:h-auto sm:aspect-w-2 sm:aspect-h-3">
+            <img :src="require(`/assets/gallery/photos/${photo.img}`)" :alt="photo.title" class="w-full h-full object-center object-cover" />
+          </div>
+          <h3 class="my-4 text-base font-semibold text-gray-900">
+            {{ photo.title }}
+          </h3>
+        </div>
+      </div>
+    </div>
   </Article>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $content }) {
+    const photos = await $content('gallery/photos')
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return {
+      photos
+    }
+  },
   head () {
     return {
       title: 'Photos'
